4754fac75162839b24879dd1a6dc11f831011fe0
Vlad Mihai <122361090+Vladmihai03@users.noreply.github.com>
2024-06-03 18:48:07 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,56 +1,50 @@
 ﻿
-//Bilet #7 / (120 min) 14.06.2020
+//Bilet #13 / (120 min) 15.06.2020
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
 #include <string.h>
 
 typedef struct {
-    int etaj;
-    char* roomType;
-    char* hotel;
-    float price;
     int cod;
-} camera;
+    char* denumire;
+    char* marca;
+    float price;
+    int cate;
+} depozit;
 
-typedef struct {
-    camera inf;
+typedef struct nodarb {
+    depozit inf;
     struct nodarb* right, * left;
 } nodarb;
 
-typedef struct {
-    camera inf;
-    struct nodLD* prev, * next;
-} nodLD;
-
-nodarb* creare(camera c, nodarb* dr, nodarb* st) {
+nodarb* creare(depozit d, nodarb* st, nodarb* dr) {
     nodarb* nou = (nodarb*)malloc(sizeof(nodarb));
-    nou->inf.etaj = c.etaj;
-    nou->inf.roomType = (char*)malloc((strlen(c.roomType) + 1) * sizeof(char));
-    strcpy(nou->inf.roomType, c.roomType);
-    nou->inf.hotel = (char*)malloc((strlen(c.hotel) + 1) * sizeof(char));
-    strcpy(nou->inf.hotel, c.hotel);
-    nou->inf.price = c.price;
-    nou->inf.cod = c.cod;
-    nou->right = dr;
+    nou->inf.cod = d.cod;
+    nou->inf.denumire = (char*)malloc((strlen(d.denumire) + 1) * sizeof(char));
+    strcpy(nou->inf.denumire, d.denumire);
+    nou->inf.marca = (char*)malloc((strlen(d.marca) + 1) * sizeof(char));
+    strcpy(nou->inf.marca, d.marca);
+    nou->inf.price = d.price;
+    nou->inf.cate = d.cate;
     nou->left = st;
+    nou->right = dr;
     return nou;
 }
 
-nodarb* inserare(nodarb* root, camera c) {
+nodarb* inserare(nodarb* root, depozit d) {
     if (root == NULL) {
-        return creare(c, NULL, NULL);
+        return creare(d, NULL, NULL);
     }
-
-    if (c.cod < root->inf.cod) {
-        root->left = inserare(root->left, c);
+    if (d.cod > root->inf.cod) {
+        root->right = inserare(root->right, d);
     }
-    else if (c.cod > root->inf.cod) {
-        root->right = inserare(root->right, c);
+    else if (d.cod < root->inf.cod) {
+        root->left = inserare(root->left, d);
     }
     else {
-        printf("\n\nExista deja aceasta valoare");
+        printf("\nAvem deja inregistrare cu acest cod\n");
     }
     return root;
 }
@@ -58,171 +52,180 @@ nodarb* inserare(nodarb* root, camera c) {
 void inordine(nodarb* root) {
     if (root != NULL) {
         inordine(root->left);
-        printf("\nEtaj: %d, RoomType: %s, Hotel: %s, Price: %5.2f, Cod: %d", root->inf.etaj, root->inf.roomType, root->inf.hotel, root->inf.price, root->inf.cod);
+        printf("\nDenumire: %s, Marca: %s, Pret: %5.2f, NumarExemplare: %d, Cod: %d", root->inf.denumire, root->inf.marca, root->inf.price, root->inf.cate, root->inf.cod);
         inordine(root->right);
     }
 }
 
-void dezalocare(nodarb* root) {
-    if (root != NULL) {
-        nodarb* st = root->left;
-        nodarb* dr = root->right;
-        free(root->inf.roomType);
-        free(root->inf.hotel);
-        free(root);
-        dezalocare(st);
-        dezalocare(dr);
-    }
-}
-
-void inserareLD(nodLD** cap, nodLD** coada, camera c) {
-    nodLD* nou = (nodLD*)malloc(sizeof(nodLD));
-    nou->inf.etaj = c.etaj;
-    nou->inf.roomType = (char*)malloc((strlen(c.roomType) + 1) * sizeof(char));
-    strcpy(nou->inf.roomType, c.roomType);
-    nou->inf.hotel = (char*)malloc((strlen(c.hotel) + 1) * sizeof(char));
-    strcpy(nou->inf.hotel, c.hotel);
-    nou->inf.price = c.price;
-    nou->inf.cod = c.cod;
-    nou->next = NULL;
-    nou->prev = NULL;
-    if (*coada == NULL && *cap == NULL) {
-        *coada = nou;
-        *cap = nou;
+void printGivenLevel(nodarb* root, int level) {
+    if (root == NULL) return;
+    if (level == 1) {
+        printf("\nDenumire: %s, Marca: %s, Pret: %5.2f, NumarExemplare: %d, Cod: %d", root->inf.denumire, root->inf.marca, root->inf.price, root->inf.cate, root->inf.cod);
     }
     else {
-        nodLD* temp = *cap;
-        while (temp->next != NULL) {
-            temp = temp->next;
-        }
-        temp->next = nou;
-        nou->prev = temp;
-        *coada = nou;
+        printGivenLevel(root->left, level - 1);
+        printGivenLevel(root->right, level - 1);
     }
 }
 
-void arbore2LD(nodarb* root, nodLD** cap, nodLD** coada, int cod) {
-    if (root == NULL) return;
-
-    arbore2LD(root->left, cap, coada, cod);
-    if (root->inf.cod > cod) {
-        inserareLD(cap, coada, root->inf);
-    }
-    arbore2LD(root->right, cap, coada, cod);
-}
-
-void afisareLD(nodLD* cap) {
-    nodLD* temp = cap;
-    while (temp != NULL) {
-        printf("\nEtaj: %d, RoomType: %s, Hotel: %s, Price: %5.2f, Cod: %d", temp->inf.etaj, temp->inf.roomType, temp->inf.hotel, temp->inf.price, temp->inf.cod);
-        temp = temp->next;
-    }
-}
-
-void inserarePrioritate(nodLD** cap, nodLD** coada, camera c, int cod) {
-    nodLD* current = *cap;
-    while (current && current->inf.cod != cod) {
-        current = current->next;
-    }
-    if (current != NULL) {
-        nodLD* nou = (nodLD*)malloc(sizeof(nodLD));
-        nou->inf.etaj = c.etaj;
-        nou->inf.roomType = (char*)malloc((strlen(c.roomType) + 1) * sizeof(char));
-        strcpy(nou->inf.roomType, c.roomType);
-        nou->inf.hotel = (char*)malloc((strlen(c.hotel) + 1) * sizeof(char));
-        strcpy(nou->inf.hotel, c.hotel);
-        nou->inf.price = c.price;
-        nou->inf.cod = c.cod;
-
-        if (current == *cap && current == *coada) {
-            current->next = nou;
-            nou->prev = current;
-            nou->next = NULL;
-            *coada = nou;
+nodarb* cautare(nodarb* root, int cod) {
+    if (root != NULL) {
+        if (root->inf.cod == cod) {
+            return root;
         }
-        else if (current == *cap) {
-            nodLD* urm = current->next;
-            urm->prev = nou;
-            nou->next = urm;
-            current->next = nou;
-            nou->prev = current;
-        }
-        else if (current == *coada) {
-            nou->next = NULL;
-            nou->prev = current;
-            current->next = nou;
-            *coada = nou;
+        else if (root->inf.cod < cod) {
+            return cautare(root->right, cod);
         }
         else {
-            nodLD* urm = current->next;
-            current->next = nou;
-            urm->prev = nou;
-            nou->prev = current;
-            nou->next = urm;
+            return cautare(root->left, cod);
         }
     }
     else {
-        printf("\n\nNu exista camera cu acest cod");
+        return NULL;
     }
 }
 
-void dezalocareLD(nodLD* cap) {
-    nodLD* temp = cap;
-    while (temp != NULL) {
-        nodLD* aux = temp->next;
-        free(temp->inf.roomType);
-        free(temp->inf.hotel);
-        free(temp);
-        temp = aux;
+void arbore2Vect(nodarb* root, depozit* vect, int* nrElem) {
+    if (root == NULL) return;
+
+    vect[*nrElem].cod = root->inf.cod;
+    vect[*nrElem].denumire = (char*)malloc((strlen(root->inf.denumire) + 1) * sizeof(char));
+    strcpy(vect[*nrElem].denumire, root->inf.denumire);
+    vect[*nrElem].marca = (char*)malloc((strlen(root->inf.marca) + 1) * sizeof(char));
+    strcpy(vect[*nrElem].marca, root->inf.marca);
+    vect[*nrElem].price = root->inf.price;
+    vect[*nrElem].cate = root->inf.cate;
+    (*nrElem)++;
+
+    if (root->left != NULL) {
+        arbore2Vect(root->left, vect, nrElem);
+    }
+    else if (root->right != NULL) {
+        arbore2Vect(root->right, vect, nrElem);
     }
 }
+int maxim(int a, int b) {
+    return(a > b) ? a : b;
+}
+
+int nrNiveluri(nodarb* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    int st = nrNiveluri(root->left);
+    int dr = nrNiveluri(root->right);
+    return 1 + maxim(st, dr);
+}
+
+int countNodesLevel(nodarb* root, int level) {
+    if (root == NULL) return 0;
+    if (level == 1) {
+        return 1;
+    }
+    else {
+        int st = countNodesLevel(root->left, level - 1);
+        int dr = countNodesLevel(root->right, level - 1);
+        return st + dr;
+    }
+}
+
+void maxLevelNodes(nodarb* root, int height, int* max, int* level) {
+    for (int i = 1; i <= height; i++) {
+        int counts = countNodesLevel(root, i);
+        if (counts > *max) {
+            *max = counts;
+            *level = i;
+        }
+    }
+}
+
+
+nodarb* removeLeafs(nodarb* root) {
+
+    if (root == NULL) {
+        return NULL;
+    }
+    
+    if (root->left == NULL && root->right == NULL) {
+        free(root->inf.denumire);
+        free(root->inf.marca);
+        free(root);
+        return NULL;
+    }
+   
+    if (root->left != NULL) {
+        root->left = removeLeafs(root->left);
+    }
+    if (root->right != NULL) {
+        root->right = removeLeafs(root->right);
+    }
+    return root;
+}
+
 
 int main() {
     FILE* f = fopen("fisier.txt", "r");
-    int nrCamere;
-    fscanf(f, "%d", &nrCamere);
+
+
+    int nrStud;
+    fscanf(f, "%d", &nrStud);
+
     nodarb* root = NULL;
-    camera c;
+    depozit d;
     char buffer1[50], buffer2[50];
-    nodLD* cap = NULL;
-    nodLD* coada = NULL;
 
-    for (int i = 0; i < nrCamere; i++) {
-        fscanf(f, "%d", &c.etaj);
+    for (int i = 0; i < nrStud; i++) {
+        fscanf(f, "%d", &d.cod);
         fscanf(f, "%s", buffer1);
-        c.roomType = (char*)malloc((strlen(buffer1) + 1) * sizeof(char));
-        strcpy(c.roomType, buffer1);
+        d.denumire = (char*)malloc((strlen(buffer1) + 1) * sizeof(char));
+        strcpy(d.denumire, buffer1);
         fscanf(f, "%s", buffer2);
-        c.hotel = (char*)malloc((strlen(buffer2) + 1) * sizeof(char));
-        strcpy(c.hotel, buffer2);
-        fscanf(f, "%f", &c.price);
-        fscanf(f, "%d", &c.cod);
-        root = inserare(root, c);
-        free(c.roomType);
-        free(c.hotel);
+        d.marca = (char*)malloc((strlen(buffer2) + 1) * sizeof(char));
+        strcpy(d.marca, buffer2);
+        fscanf(f, "%f", &d.price);
+        fscanf(f, "%d", &d.cate);
+        root = inserare(root, d);
+        free(d.denumire); // Free after inserare since inserare makes a copy
+        free(d.marca); // Free after inserare since inserare makes a copy
     }
+
     fclose(f);
-
     inordine(root);
-    printf("\n\n");
-    arbore2LD(root, &cap, &coada, 1005);
-    afisareLD(cap);
+    printf("\n");
+    printGivenLevel(root, 3);
+    printf("\n");
 
-    camera g;
-    g.etaj = 2;
-    g.roomType = (char*)malloc((strlen("Single") + 1) * sizeof(char));
-    strcpy(g.roomType, "Single");
+    nodarb* cautat = cautare(root, 8);
+    if (cautat != NULL) {
+        printf("\nDenumire: %s, Marca: %s, Pret: %5.2f, NumarExemplare: %d, Cod: %d", cautat->inf.denumire, cautat->inf.marca, cautat->inf.price, cautat->inf.cate, cautat->inf.cod);
+        printf("\n");
 
-    g.hotel = (char*)malloc((strlen("Marriot") + 1) * sizeof(char));
-    strcpy(g.hotel, "Marriot");
-    g.price = 120.23;
-    g.cod = 1017;
-    printf("\n\n");
-    inserarePrioritate(&cap, &coada, g, 1009);
-    afisareLD(cap);
+        depozit* vect = (depozit*)malloc(nrStud * sizeof(depozit));
+        int nrElem = 0;
+        arbore2Vect(cautat, vect, &nrElem);
 
-    dezalocare(root);
-    dezalocareLD(cap);
+        printf("\nPath from node to root:\n");
+        for (int i = 0; i < nrElem; i++) {
+            printf("Cod: %d, Denumire: %s, Marca: %s, Pret: %.2f, NumarExemplare: %d\n", vect[i].cod, vect[i].denumire, vect[i].marca, vect[i].price, vect[i].cate);
+            free(vect[i].denumire);
+            free(vect[i].marca);
+        }
+        free(vect);
+    }
+    else {
+        printf("\nNode with cod 3 not found.\n");
+    }
+    printf("\n");
+    int niveluri = nrNiveluri(root);
+    printf("\nNumar niveluri: %d", niveluri);
+    printf("\n");
+    int maxim = 0;
+    int level = 0;
+    maxLevelNodes(root, niveluri, &maxim, &level);
+    printf("\n\nCele mai multe noduri se regasesc la nivelul %d si sutn exact %d\n", level, maxim);
+    removeLeafs(root);
+    inordine(root);
+    printf("\n");
 
     return 0;
 }
